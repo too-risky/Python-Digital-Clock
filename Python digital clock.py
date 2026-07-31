417d70c8e2f5b2c4a37e631b3e433e8905b88221
Aradhya <aradhyakurve36@gmail.com>
2026-07-31 16:22:36 +0530

    Add files via upload

---
+++ Python digital clock.py
@@ -0,0 +1,48 @@
+# Python digital clock
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTime, QTimer, Qt
+from PyQt5.QtGui import QFont, QFontDatabase
+
+class Digitalclock(QWidget) :
+    def __init__(self) :
+        super().__init__()
+        self.time_label = QLabel(self)
+        self.timer = QTimer(self)
+        self.initUI()
+
+    def initUI(self) :
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(700, 200, 500, 100)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 130px;"
+                                      "color: #2ebf0b")
+        self.setStyleSheet("background-color: black;")
+
+        font = QFontDatabase.addApplicationFont("C:/Users/Vivek/OneDrive/Desktop/Aradhya/Coding/DS-DIGIT.TTF")
+        font_family = QFontDatabase.applicationFontFamilies(font)[0]
+        my_font = QFont(font_family, 130)
+        self.time_label.setFont(my_font)
+
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+
+        self.update_time()
+        
+
+    def update_time(self) :
+        currnt_time = QTime.currentTime().toString("hh:mm:ss AP")
+        self.time_label.setText(currnt_time)
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    clock = Digitalclock()
+    clock.show()
+    sys.exit(app.exec_())
+
+
